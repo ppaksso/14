0908fd7032d5d81fc0f12b707dc2b5b904e8b387
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-12-06 17:31:44 +0900

    02

--- main.c
+++ main.c
@@ -10,25 +10,24 @@ struct Book{
 };
 
 int main(int argc, char *argv[]) {
-	int i;
-	struct Book **bookshelf;
-	bookshelf=(struct Book**)malloc(3*sizeof(struct Book*));
+	struct Book *p;
 	
-	for(i=0;i<3;i++)
-	bookshelf[i]=(struct Book*)malloc(10*sizeof(struct Book));
+	p=(struct Book*)malloc(2*sizeof(struct Book));
 	
-	bookshelf[1][3].number =5;
-	strcpy(bookshelf[1][3].title,"C++ Programming");
+	if(p==NULL){
+		printf("메모리할당오류\n");
+		return;
+	} 
 	
-	(bookshelf[2]+4)->number =3;
-	strcpy((bookshelf[2]+4)->title,"Communication Theory");
+	p->number = 1;
+	strcpy(p->title,"C Programming");
 	
-	printf("book(1,3) : %i, %s\n",(bookshelf[1]+3)->number,(bookshelf[1]+3)->title);
-	printf("book(2,4) : %i, %s\n",bookshelf[2][4].number,bookshelf[2][4].title);
+	(p+1)->number = 2;
+	strcpy((p+1)->title,"Electronics");
 	
-	for(i=0;i<3;i++)
-	  free(bookshelf[i]);
-	  free(bookshelf);
+	free(p);
+
+
 
 	return 0;
 }
